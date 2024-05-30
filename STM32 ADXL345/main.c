7957341da7713d3307799ab0d5ed286620fc166c
Faariq-Siddiqui <160421735038@mjcollege.ac.in>
2024-05-30 15:20:45 +0530

    Cleaner and better documented code

--- STM32 ADXL345/main.c
+++ STM32 ADXL345/main.c
@@ -1,32 +1,14 @@
-/* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-/* Includes ------------------------------------------------------------------*/
+/* Including  Header Files */
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+/* Defining Device Defaults*/
 #define ADXL345_DEV_ADDRESS 0x53 << 1
-#define ADXL345_REG_BW_RATE 0x2C
 #define ADXL345_REG_DEVID 0x00
 
+/* Setting the registers which prompt ADXL345 for reading vibrations*/
+#define ADXL345_REG_BW_RATE 0x2C
 #define ADXL345_REG_POWER_CTL 0x2D
 #define ADXL345_MEASURE_MODE 0x08
 #define ADXL345_REG_THRESHOLD_ACTIVE 0x24
@@ -34,6 +16,7 @@
 #define ADXL345_REG_TAP_AXES 0x2A
 #define ADXL345_REG_DATA_FORMAT 0x31
 
+/* Registers which hold the readings */
 #define ADXL345_REG_X0 0x32
 #define ADXL345_REG_X1 0x33
 #define ADXL345_REG_Y0 0x34
@@ -41,17 +24,12 @@
 #define ADXL345_REG_Z0 0x36
 #define ADXL345_REG_Z1 0x37
 
-#define ADXL345_OFSX 0x1E
-#define ADXL345_OFSY 0x1F
-#define ADXL345_OFSZ 0x20
-
+/*miscellaneous macros for use */
 #define ADXL345_HIGH_REG_VALUE 0xff
-#define ACCELERATION_DUE_TO_GRAVITY 9.8
 #define SCALE_FACTOR 4.3
-/* USER CODE END Includes */
+#define ACCELERATION_DUE_TO_GRAVITY 9.8
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+/* creates structures to store values in registers */
 struct acc_reg_data{
 	int16_t reg_x;
 	int16_t reg_y;
@@ -64,43 +42,30 @@ struct output_reg_data{
 	float reg_z;
 } output_in_G, output_in_MetersPerSecondSquared;
 
-//struct offset_reg{
-//	uint8_t reg_x;
-//	uint8_t reg_y;
-//	uint8_t reg_z;
-//} offset;
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
+/* Stores a byte of data */
 uint8_t dataStoreBuffer[1];
-/* USER CODE END PD */
+uint8_t temp;
 
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
 
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 I2C_HandleTypeDef hi2c1;
 
 UART_HandleTypeDef huart2;
 
-/* USER CODE BEGIN PV */
 
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-static void MX_GPIO_Init(void);
-static void MX_USART2_UART_Init(void);
-static void MX_I2C1_Init(void);
-/* USER CODE BEGIN PFP */
+void MX_GPIO_Init(void);
+void MX_USART2_UART_Init(void);
+void MX_I2C1_Init(void);
 
-/* USER CODE END PFP */
-uint8_t temp;
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
+
+
+/**************************************************
+ * @brief:  This function writes to a single register of slave device whose address is supplied as a parameter under I2C Protocol.
+ * @parameter:  MemAddress -> ( unsigned 16 bit address ) usually supplied as a hex value. This is the address of the register of the slave device to which we are writing.
+ * @parameter:  Data -> (unsigned 8 bit value) usually supplied as a hex value. This is the value that one wants to set in that register
+ * @return value: Null
+**************************************************/
+
 void ADXL345_Write_To_Reg(uint16_t MemAddress, uint8_t Data){
 	if (HAL_I2C_Mem_Write(&hi2c1, ADXL345_DEV_ADDRESS, MemAddress, 1, &Data, 1, 100) == HAL_OK )
 	{
@@ -113,6 +78,7 @@ void ADXL345_Write_To_Reg(uint16_t MemAddress, uint8_t Data){
 	}
 }
 
+// Uses ADXL345_Write_To_Reg() to write to registers of ADXL345, thus initialising it for use.
 void ADXL345_Init(void){
 
 	/****************************************************************
@@ -135,10 +101,13 @@ void ADXL345_Init(void){
 	ADXL345_Write_To_Reg(ADXL345_REG_POWER_CTL, 0x3B); // Power Control Register
 	ADXL345_Write_To_Reg(ADXL345_REG_BW_RATE, 0x0A); // Set 0x0A for 100Hz which is for I2C Protocol
 
-//	ADXL345_Write_To_Reg(ADXL345_OFSX, 0x40);
-//	ADXL345_Write_To_Reg(ADXL345_OFSY, 0x40);
-//	ADXL345_Write_To_Reg(ADXL345_OFSZ, 0x40);
-}
+
+/**************************************************
+* @brief:  This function reads from a single register of slave device whose address is supplied as a parameter under I2C Protocol.
+* @parameter:  MemAddress -> ( unsigned 16 bit address ) usually supplied as a hex value. This is the address of the register of the slave device being read from.
+* @return value: dataStoreBuffer[0], returns the 1 and only byte stored in dataStoreBuffer. This has the data read from the register
+* 				 which maybe used directly or maybe stored in a variable for later use.
+**************************************************/
 
 uint8_t ADXL345_Read_From_Reg(uint16_t MemAddress){
 	if (HAL_I2C_Mem_Read(&hi2c1, ADXL345_DEV_ADDRESS, MemAddress, 1, dataStoreBuffer, 1, 100) == HAL_OK )
@@ -153,23 +122,10 @@ uint8_t ADXL345_Read_From_Reg(uint16_t MemAddress){
 	return dataStoreBuffer[0];
 }
 
+// Uses ADXL345_Read_From_Reg() function to read from registers.
 void read_XYZ(void){
 
 	uint8_t x0, y0, z0, x1, y1, z1;
-
-//	x0 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_X0);
-//	x1 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_X1);
-//
-//	y0 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_Y0);
-//	y1 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_Y1);
-//
-//	z0 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_Z0);
-//	z1 = ADXL345_HIGH_REG_VALUE - ADXL345_Read_From_Reg(ADXL345_REG_Z1);
-
-//	offset.reg_x = ADXL345_Read_From_Reg(ADXL345_OFSX);
-//	offset.reg_y = ADXL345_Read_From_Reg(ADXL345_OFSY);
-//	offset.reg_z = ADXL345_Read_From_Reg(ADXL345_OFSZ);
-
 
 	x0 = ADXL345_Read_From_Reg(ADXL345_REG_X0);
 	x1 = ADXL345_Read_From_Reg(ADXL345_REG_X1);
@@ -180,6 +136,9 @@ void read_XYZ(void){
 	z0 = ADXL345_Read_From_Reg(ADXL345_REG_Z0);
 	z1 = ADXL345_Read_From_Reg(ADXL345_REG_Z1);
 
+	// First combines two bytes into a 16 bit value by OR-ing the individual 8 bits and then converting them to integer or in other words a signed value.
+	// This is where we convert the unsigned value to signed value.
+	//THen we store this in sensorData variable struct which we created above.
 	sensorData.reg_x = (int)(x1 << 8 | x0);
 	sensorData.reg_y = (int)(y1 << 8 | y0);
 	sensorData.reg_z = (int)(z1 << 8 | z0);
@@ -196,54 +155,30 @@ float convert_output_to_MeterPerSecondSqaure(uint16_t registerValue){
 	return register_value_in_MeterPerSecondSqaure;
 }
 
+// This helps provide pritnf output to the console shell in STM32CubeIDE via VirtualCOM through USART2.
 int _write(int file, char *ptr, int len){
 	HAL_UART_Transmit(&huart2, (uint8_t*)ptr, len, HAL_MAX_DELAY);
 	return len;
 }
-/* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
 	float total_value_in_G = 0;
 	float total_value_in_MeterPerSecondSquare = 0;
 	int stopFlag = 1;
-  /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	HAL_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	SystemClock_Config();
 
-  /* USER CODE BEGIN Init */
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_I2C1_Init();
 
-  /* USER CODE END Init */
+    ADXL345_Init();
+    while (1){
 
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_I2C1_Init();
-  /* USER CODE BEGIN 2 */
-  ADXL345_Init();
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-    {
-      /* USER CODE END WHILE */
   	  for(int numberOfReadings = 1; numberOfReadings <= 25; numberOfReadings++){
 
   			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
@@ -272,10 +207,6 @@ int main(void)
   			printf("\r\n");
   			printf("Total Value: %f m/s^2\r\n", total_value_in_MeterPerSecondSquare);
   			printf("\r\n");
-//  		printf("Offset Register values: \r\n");
-//  		printf("\r\n");
-//  		printf("offset reg x: %u, offset reg y: %u, offset reg z: %u ", offset.reg_x, offset.reg_y, offset.reg_z);
-//  		printf("\r\n");
   			printf("-----------------------------------------------------------------------------------------------------------\r\n");
 
   			HAL_Delay(500);
@@ -286,23 +217,18 @@ int main(void)
   	  while(stopFlag != 0){
 
   	  }
-      /* USER CODE BEGIN 3 */
+
     }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -315,7 +241,7 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
+    */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -334,16 +260,8 @@ void SystemClock_Config(void)
   * @param None
   * @retval None
   */
-static void MX_I2C1_Init(void)
-{
+void MX_I2C1_Init(void){
 
-  /* USER CODE BEGIN I2C1_Init 0 */
-
-  /* USER CODE END I2C1_Init 0 */
-
-  /* USER CODE BEGIN I2C1_Init 1 */
-
-  /* USER CODE END I2C1_Init 1 */
   hi2c1.Instance = I2C1;
   hi2c1.Init.ClockSpeed = 100000;
   hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
@@ -357,10 +275,6 @@ static void MX_I2C1_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
-
-  /* USER CODE END I2C1_Init 2 */
-
 }
 
 /**
@@ -368,16 +282,9 @@ static void MX_I2C1_Init(void)
   * @param None
   * @retval None
   */
-static void MX_USART2_UART_Init(void)
+void MX_USART2_UART_Init(void)
 {
 
-  /* USER CODE BEGIN USART2_Init 0 */
-
-  /* USER CODE END USART2_Init 0 */
-
-  /* USER CODE BEGIN USART2_Init 1 */
-
-  /* USER CODE END USART2_Init 1 */
   huart2.Instance = USART2;
   huart2.Init.BaudRate = 115200;
   huart2.Init.WordLength = UART_WORDLENGTH_8B;
@@ -390,10 +297,6 @@ static void MX_USART2_UART_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN USART2_Init 2 */
-
-  /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
@@ -401,11 +304,9 @@ static void MX_USART2_UART_Init(void)
   * @param None
   * @retval None
   */
-static void MX_GPIO_Init(void)
+void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
@@ -433,27 +334,20 @@ static void MX_GPIO_Init(void)
   HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
 }
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+void Error_Handler(void){
+
   __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+  while (1){
+
+  	  }
+
+ 	 }
 }
 
 #ifdef  USE_FULL_ASSERT
@@ -466,9 +360,6 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+
 }
 #endif /* USE_FULL_ASSERT */
